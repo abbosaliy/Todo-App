a5b16ca20f0a5e1aa397528ee4b231c6c0d777ac
abbosaliy <abbosbekanvarjonov8@gmail.com>
2025-04-26 22:21:14 +0200

    Check box function and medi query added

---
+++ src/components/TodoApp.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react';
+import './TodoApp.css';
+import TodoHeader from './TodoHeader';
+import TodoItem from './TodoListeItem';
+
+function TodoApp() {
+	const [todos, setTodos] = useState([]);
+
+	function addTodoItemTodoListe(todoItem) {
+		const updatedTodos = [...todos];
+		updatedTodos.push(todoItem);
+		setTodos(updatedTodos);
+	}
+
+	function removeTodo(todoIndex) {
+		console.log(todoIndex);
+		const updatedTodos = todos.filter((element, index) => index !== todoIndex);
+		setTodos(updatedTodos);
+	}
+
+	function handleChangeCheckBox(index) {
+		console.log(index);
+
+		const updatedTodos = [...todos];
+		updatedTodos[index].done = !updatedTodos[index].done;
+		setTodos(updatedTodos);
+	}
+
+	return (
+		<div className="todo">
+			<TodoHeader addTodoItemTodoListe={addTodoItemTodoListe} />
+
+			<div className="todo-liste">
+				{todos.length === 0 && (
+					<h2 className="todo-liste__text">Keine Todos</h2>
+				)}
+
+				{todos.map((todo, index) => (
+					<TodoItem
+						key={index}
+						todo={todo}
+						removeTodo={() => removeTodo(index)}
+						handleChangeCheckBox={() => handleChangeCheckBox(index)}
+					/>
+				))}
+			</div>
+		</div>
+	);
+}
+
+export default TodoApp;
---
+++ src/components/TodoHeader.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+import './TodoHeader.css';
+
+function TodoHeader({ addTodoItemTodoListe }) {
+	const [inputValue, setInputValue] = useState('');
+
+	function handleInputChange(changeValue) {
+		setInputValue(changeValue.target.value);
+	}
+
+	function addHandleClick() {
+		if (!inputValue) return;
+		addTodoItemTodoListe({ content: inputValue, done: false });
+		setInputValue('');
+	}
+
+	return (
+		<div className="todo-header">
+			<h2 className="todo-name">Todo App</h2>
+			<div className="todo-form">
+				<input
+					tuype="text"
+					value={inputValue}
+					onChange={handleInputChange}
+					className="todo__input"
+					placeholder="Todo hinzufugen"
+				/>
+				<button onClick={addHandleClick} className="todo__add-button">
+					Add
+				</button>
+			</div>
+		</div>
+	);
+}
+
+export default TodoHeader;
